58255f171035b4d3cdc5ae2bd9593a45c2d51efe
mathluva <mathluvacoding@gmail.com>
2020-08-30 16:38:12 -0400

    update scrape_mars file

--- scrape_mars.py
+++ scrape_mars.py
@@ -6,158 +6,127 @@ import pandas as pd
 import requests
 import time
 
+def init_browser():
+    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    browser = Browser('chrome', **executable_path, headless=False)
+    return browser
 
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-browser = Browser('chrome', **executable_path, headless=False)
-
-
-# URL of page to be scraped
-url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
 
 #print latest title
-def scrape1(url):   
+def scrape1():  
+    browser = init_browser() 
+    url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.visit(url)
+
     time.sleep(1)
+    #scrape page into soup
     html = browser.html
     soup = bs(html, 'html.parser')
     titles=soup.find_all('div', class_="content_title")
+
+    #print latest title
     latest_title = titles[1]
     latest_title = latest_title.a.text
-    print(latest_title)
 
-scrape1(url)
-
-#scrape latest news paragraph text
-def scrape2(url):
-    browser.visit(url)
-    time.sleep(1)
-    html = browser.html
-    soup = bs(html, 'html.parser')
+    #print latest title paragraph
     paragraph=soup.find('div', class_='article_teaser_body')
     latest_paragraph=paragraph.text
-    print(latest_paragraph)
+    
+    #close the browser after scraping
+    browser.quit
 
-scrape2(url)
+    print("The latest news title and paragraph text on the Nasa Mars News Site:")
+    print(f" '{latest_title}'")
+    print(f" {latest_paragraph}")
 
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+#scrape1()
 
-def scrape3(url):
+def scrape2():
+    browser = init_browser() 
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url)
+
+    time.sleep(1)
+
+    #scrape page into soup
     html = browser.html
     soup = bs(html, 'html.parser')
-    results=soup.prettify()
+
     images= soup.find_all('div', class_="img")
     source_url = images[0].img["src"]
     featured_image_url =(f"https://www.jpl.nasa.gov{source_url}")
+
+    browser.quit
+
     print(featured_image_url)
 
-scrape3(url)
+#scrape2()
 
+def scrape3():
+    browser = init_browser() 
+    url="https://space-facts.com/mars/"
+    browser.visit(url)
 
-
-url="https://space-facts.com/mars/"
-tables = pd.read_html(url)
-#tables
-
-
-
-
-tables = pd.read_html(str(url))[0]
-tables
-
-
-
-
-url="https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-browser.visit(url)
-
-
-
-
-html = browser.html
-soup = bs(html, 'html.parser')
-
-
-
-
-#print(soup.prettify())
-
-
-
-
-images=soup.find_all('a', class_="itemLink product-item")
-print(images)
-
-
-
-
-image_title=soup.find_all('h3')
-image_title
-
-
-
-
-#print each title as text
-image_title[1].text
-
-
-
-
-#first url
-url=images[6]['href']
-click_url=f"https://astrogeology.usgs.gov/{url}"
-click_url
-
-
-
-
-browser.visit(click_url)
-html=browser.html
-soup=bs(html,'html.parser')
-
-
-
-
-source_url=soup.find('li')
-picture_url=source_url.a['href']
-picture_url
-
-
-
-
-title_list=[]
-url_list=[]
-dictionary=[]
-images = images[: : 2]
-for i in range(len(image_title)):
-    title_list.append(image_title[i].text)
-    url=images[i]['href']
-    click_url=f"https://astrogeology.usgs.gov/{url}"
-    browser.visit(click_url)
     time.sleep(1)
+    tables = pd.read_html(url)
+
+    #scrape table containing facts about mars
+    #convert data to html table string
+    tables = pd.read_html(str(url))[0]
+
+    browser.quit
+    
+    print(tables)
+#scrape3()
+
+def scrape4():
+    browser = init_browser() 
+    url="https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+    time.sleep(1)
+
+    #scrape page into soup
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    images=soup.find_all('a', class_="itemLink product-item")
+
+    image_title=soup.find_all('h3')
+
+    url=images[6]['href']
+
+    click_url=f"https://astrogeology.usgs.gov/{url}"
+
+    browser.quit
+
+    browser.visit(click_url)
     html=browser.html
     soup=bs(html,'html.parser')
+
     source_url=soup.find('li')
     picture_url=source_url.a['href']
-    url_list.append(picture_url)
-    dictionary.append({"title":image_title[i].text, "img_url":picture_url})
 
 
+    title_list=[]
+    url_list=[]
+    dictionary=[]
+    images = images[: : 2]
+    for i in range(len(image_title)):
+        title_list.append(image_title[i].text)
+        url=images[i]['href']
+        click_url=f"https://astrogeology.usgs.gov/{url}"
+        browser.visit(click_url)
+        time.sleep(1)
+        html=browser.html
+        soup=bs(html,'html.parser')
+        source_url=soup.find('li')
+        picture_url=source_url.a['href']
+        url_list.append(picture_url)
+        dictionary.append({"title":image_title[i].text, "img_url":picture_url})
 
+        browser.quit
 
-#print(title_list)
+    print(dictionary)
 
-
-
-
-#print(url_list)
-
-
-
-
-print(dictionary)
-
-
-
-
-
+scrape4()    
